883deb9af0040ccc68088862851137655a00c828
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-06-08 16:09:24 +0300

    Update CraftRise.bat

--- CraftRise.bat
+++ CraftRise.bat
@@ -1,6 +1,6 @@
 color a
 cd C:\Users\%USERNAME%\Downloads
-curl https://www.craftrise.com.tr/launcher/CraftRise.exe -o CraftRise.exe
+curl https://www.craftrise.com.tr/launcher/CraftRise.exe -o Craftrise.exe
 cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs
 mkdir CraftRise
 move C:\Users\%USERNAME%\Downloads\CraftRise.exe C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\CraftRise\
