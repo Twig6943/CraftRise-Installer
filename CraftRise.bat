bdbe76d978a2a592a0241983b1a142197870c4be
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-06-08 16:27:10 +0300

    Update CraftRise.bat

--- CraftRise.bat
+++ CraftRise.bat
@@ -1,6 +1,7 @@
 color a
 cd "C:\Users\%USERNAME%\Downloads"
 curl https://www.craftrise.com.tr/launcher/CraftRise.exe -o CraftRise.exe
+curl https://github.com/Twig6943/CraftRise-Installer/raw/main/CraftRise.lnk -o CraftRise.lnk
 cd "C:\Program Files (x86)"
 mkdir CraftRise
 move "C:\Users\%USERNAME%\Downloads\CraftRise.exe" "C:\Program Files (x86)\CraftRise"
