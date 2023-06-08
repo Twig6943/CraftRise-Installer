c4c2c549325ff5ed66ea8690e20b22782cd61125
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-06-08 16:25:33 +0300

    Update CraftRise.bat

--- CraftRise.bat
+++ CraftRise.bat
@@ -1,11 +1,11 @@
 color a
-cd C:\Users\%USERNAME%\Downloads
+cd "C:\Users\%USERNAME%\Downloads"
 curl https://www.craftrise.com.tr/launcher/CraftRise.exe -o CraftRise.exe
 cd "C:\Program Files (x86)"
 mkdir CraftRise
 move "C:\Users\%USERNAME%\Downloads\CraftRise.exe" "C:\Program Files (x86)\CraftRise"
-cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs
+cd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 mkdir CraftRise
-move "C:\Users\%USERNAME%\Downloads\CraftRise.exe" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\CraftRise\"
+move "C:\Users\%USERNAME%\Downloads\CraftRise.lnk" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\CraftRise"
 echo The installation is done press any key to close this window.
 pause
